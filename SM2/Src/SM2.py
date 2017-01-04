4108a70556667386baa0696f90015ab5d0f53585
clock zhong <zhong.clock@gmail.com>
2017-01-04 23:52:11 +0800

    some changes

--- SM2/Src/SM2.py
+++ SM2/Src/SM2.py
@@ -1,6 +1,24 @@
 #! /usr/bin/env python
+
+class Card(object):
+    def __init__(self):
+        self.EF=2.0 #init the EF(Easiness Factor) value to 2.0
+        self.interval=1*24*3600#The first interval is 1 day(24*3600secs), and the second interval is 6 days, I(1)=1
+        
 class SM2(object):
     def __init__(self):
-        print("SM2 inited")
+        #print("SM2 inited")
         pass
+    def updateEF(self, card, easiness):
+        newEF=card.EF+(0.1-(5-easiness)*(0.08+(5-easiness)*0.02))
+        if (newEF<1.3):
+            newEF=1.3
+        card.EF=newEF
+        return newEF
+    def updateInterval(self, card, easiness):
+        if card.interval==1*24*3600:
+            card.interval =6*24*3600 #I(2)=6
+        else:
+            card.interval=card.interval*card.EF
+        return card.interval
 
--- SM2/Test/Tests.py
+++ SM2/Test/Tests.py
@@ -3,16 +3,20 @@ import sys
 import os
 sys.path.append('../Src')
 from SM2 import SM2
+from SM2 import Card
 import unittest
 
 class TestSM2(unittest.TestCase):
     def setUp(self):
-        self.mySM2=SM2()
+        self.SM2=SM2()
+        self.card=Card()
         pass
     def tearDown(self):
         pass
-    def testAAA1(self):
-        pass
+    def testEFValue(self):
+        self.assertEqual(self.card.EF, 2.0)
+        print self.SM2.updateEF(self.card, 5)
+
 
 if __name__ == '__main__':
 	unittest.main()
