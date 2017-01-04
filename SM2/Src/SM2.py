353196221db99e7bc4282e4d8f616ccee2a611b2
clock zhong <zhong.clock@gmail.com>
2017-01-05 00:29:37 +0800

    the SM2 implementation and its test is done

--- SM2/Src/SM2.py
+++ SM2/Src/SM2.py
@@ -4,21 +4,39 @@ class Card(object):
     def __init__(self):
         self.EF=2.0 #init the EF(Easiness Factor) value to 2.0
         self.interval=1*24*3600#The first interval is 1 day(24*3600secs), and the second interval is 6 days, I(1)=1
-        
-class SM2(object):
-    def __init__(self):
-        #print("SM2 inited")
-        pass
-    def updateEF(self, card, easiness):
-        newEF=card.EF+(0.1-(5-easiness)*(0.08+(5-easiness)*0.02))
+
+        #I use needRelearn to indicate whether the user need re-study this entry after anwser() it
+        # The following copied from sm2.html, 
+        #"After each repetition session of a given day repeat again all items that scored below four in the quality assessment. Continue the repetitions until all of these items score at least four."
+        self.needRelearn=False
+    def updateEF(self, easiness):
+        newEF=self.EF+(0.1-(5-easiness)*(0.08+(5-easiness)*0.02))
         if (newEF<1.3):
             newEF=1.3
-        card.EF=newEF
-        return newEF
-    def updateInterval(self, card, easiness):
-        if card.interval==1*24*3600:
-            card.interval =6*24*3600 #I(2)=6
+        self.EF=newEF
+        return self.EF
+    def updateInterval(self, easiness):
+        if self.interval==1*24*3600:
+            self.interval =6*24*3600 #I(2)=6
         else:
-            card.interval=card.interval*card.EF
-        return card.interval
+            self.interval=int(self.interval*self.EF)
+        return self.interval
+    def answer(self, easiness):
+        if (easiness<3):
+            self.interval=1*24*3600 #when the user response as 0,1 or 2, we need restart the SR process
+            self.EF=2.0                   # I don't think the EF need to be reset, but SM2 request it clearly
+            self.needRelearn=True
+            return
+
+        #should we use the newer EF to calculate the next interval? if yes, we need switch the following lines, 
+        #if not, just keep it. According to my understanding on the https://www.supermemo.com/english/ol/sm2.htm
+        #It's very possible that we need use the old EF value to calculate the next interval, and the new EF value
+        #need be used in the NEXT NEXT interval's calculation
+        self.updateInterval(easiness)
+        self.updateEF(easiness)
+
+        #check whether we need relearn this card today
+        if (easiness<4):
+            self.needRelearn=True
+
 
--- SM2/Test/Tests.py
+++ SM2/Test/Tests.py
@@ -2,21 +2,45 @@
 import sys
 import os
 sys.path.append('../Src')
-from SM2 import SM2
 from SM2 import Card
 import unittest
 
 class TestSM2(unittest.TestCase):
     def setUp(self):
-        self.SM2=SM2()
         self.card=Card()
         pass
     def tearDown(self):
         pass
-    def testEFValue(self):
+    def testInitValue(self):
         self.assertEqual(self.card.EF, 2.0)
-        print self.SM2.updateEF(self.card, 5)
+        self.assertEqual(self.card.interval, 1*24*3600)
+    def testAnswer(self):
+        #when answer easiness as 0, the EF and interval should be same as the init values
+        self.card.answer(0)
+        self.assertEqual(self.card.EF, 2.0)
+        self.assertEqual(self.card.interval, 1*24*3600)
+        #when answer easiness as 1, the EF and interval should be same as the init values
+        self.card.answer(1)
+        self.assertEqual(self.card.EF, 2.0)
+        self.assertEqual(self.card.interval, 1*24*3600)
+        #when answer easiness as 2, the EF and interval should be same as the init values
+        self.card.answer(2)
+        self.assertEqual(self.card.EF, 2.0)
+        self.assertEqual(self.card.interval, 1*24*3600)
 
+
+        self.card.answer(3)
+        #print self.card.EF
+        #print self.card.interval
+        self.assertEqual(self.card.EF, 1.86)
+        self.assertEqual(self.card.interval, 6*24*3600)
+        self.assertEqual(self.card.needRelearn, True)
+
+        self.card.answer(4)
+        #print self.card.EF
+        #print self.card.interval
+        self.assertEqual(self.card.EF, 1.86)
+        self.assertEqual(self.card.interval, int(6*24*3600*1.86))
 
 if __name__ == '__main__':
 	unittest.main()
